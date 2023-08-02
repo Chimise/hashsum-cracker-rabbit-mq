9a34663d5a20fa9b9441ba62a0abea1553ac2ef0
chisom promise <chimisepro@gmail.com>
2023-08-02 23:58:49 +0100

    feat: implement a result aggreagator or collector using rabbitmq

---
+++ sink.js
@@ -0,0 +1,20 @@
+import amqp from "amqplib";
+
+const port = process.env.PORT || 5018;
+
+async function main() {
+  const connection = await amqp.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+  const { queue } = await channel.assertQueue("result_queue");
+
+  channel.consume(queue, (msg) => {
+    const message = msg.content.toString();
+    console.log('Message: %s', message);
+    channel.ack(msg);
+  })
+
+  
+
+}
+
+main().catch((err) => console.log(err));
